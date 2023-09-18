88b97147cd590ad02a9cdca1c8998a550e4db805
developer <55893758+berkegenckaya@users.noreply.github.com>
2023-09-18 23:43:49 +0300

    Update Input.tsx

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,6 +1,7 @@
 interface InputProps {
   placeholder?: string;
   value?: string;
+  numberValue?: Number;
   type?: string;
   disabled?: boolean;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
@@ -10,6 +11,7 @@ const Input: React.FC<InputProps> = ({
   placeholder,
   value,
   type,
+  numberValue,
   disabled,
   onChange,
 }) => {
@@ -20,6 +22,7 @@ const Input: React.FC<InputProps> = ({
       value={value}
       placeholder={placeholder}
       type={type}
+      
       className="w-full p-4 text-lg bg-black border-2 border-neutral-800 rounded-md outline-none text-white focus:border-sky-500 focus:border-2 transition disabled:bg-neutral-900 disabled:opacity-70 disabled:cursor-not-allowed"
     />
   );
