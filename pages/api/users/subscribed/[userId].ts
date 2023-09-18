814b4df0a7369cea2e3c4506c733e0120374c540
developer <55893758+berkegenckaya@users.noreply.github.com>
2023-09-19 00:02:16 +0300

    Update [userId].ts

--- pages/api/users/subscribed/[userId].ts
+++ pages/api/users/subscribed/[userId].ts
@@ -15,6 +15,9 @@ export default async function handler(
     if (!currentUser || typeof currentUser.id !== "string") {
       throw new Error("Invalid ID");
     }
+    if (!userId || typeof userId !== 'string') {
+      throw new Error('Invalid ID');
+    }
 
     const existingUser = await prisma.user.findUnique({
       where: {
@@ -33,7 +36,7 @@ export default async function handler(
           has: userId,
         },
       },
-    }); console.log(subscriber)
+    }); 
     return res.status(200).json(subscriber ? subscriber : "");
    
   } catch (error) {
